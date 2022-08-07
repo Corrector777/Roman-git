d9339a4820f093b8783a00cc4ec303903e7762be
Corrector777 <rlysogor84@gmail.com>
2022-08-07 21:04:03 +0300

    vs code

--- netology-bot.py
+++ netology-bot.py
@@ -1,34 +1,51 @@
+import random
+
 HELP = """
 help - напечатать справку по программе.
 add - добавить задачу в список (название задачи запрашиваем у пользователя).
 show - напечатать все добавленные задачи."""
-today_tasks = []
-tomorrow_tasks = []
-other_tasks = []
 
+tasks = {}
 
 while True:
+
+    random_tasks = ['Погулять', 'Выпить', 'что-то еще']
+    def command_input(date, task):      
+      if date in tasks:        
+        tasks[date].append(task)
+      else:
+        tasks[date] = []
+        tasks[date].append(task)
+      print(f"Задача <{task}> добавлена на дату {date}")
+
 
     command = input("Введите команду: ")
     if command.lower().strip() == "help":
         print(HELP)
     elif command.lower().strip() == "show":
-        print(f'Задачи сегодня: {today_tasks}')
-        print(f'Задачи на завтра: {tomorrow_tasks}')
-        print(f'Иные задачи: {other_tasks}')
-    elif command.lower().strip() == "add":
-      
-      date_task = input("Введите дату выполнения задачи: ")
-      task = input ('Введите  задачу: ')
-      if date_task.lower().strip() == 'сегодня':
-        today_tasks.append(task)
-        print(f"Задача <{task}> добавлена")
-      elif date_task.lower().strip() == 'завтра':
-        tomorrow_tasks.append(task)
-        print(f'Задача <{task}> добавлена')
+      date = input('Введите дату: ')
+      if date in tasks:
+        print(f'На данную дату запланированно:')
+        for task in tasks[date]:             
+          print(f'-- {task}')
       else:
-        other_tasks.append(task)
-        print(f'Задача <{task}> добавлена')    
+        print(f'На дату: {date} пока нет задач')         
+    elif command.lower().strip() == "add": 
+      date = input("Введите дату выполнения задачи: ")
+      task = input ('Введите  задачу: ')
+      command_input(date, task)      
+      
+
+      #elif date_task.lower().strip() == 'завтра':
+        #tomorrow_tasks.append(task)
+        #print(f'Задача <{task}> добавлена')
+      #else:
+        #tasks[date].append(task)
+        #print(f'Задача <{task}> добавлена')    
+    elif command.lower().strip() == "random":  
+      date = input("Введите дату выполнения задачи: ")
+      random_task = random.choice(random_tasks)
+      command_input(date, random_task)
     elif command.lower().strip() == 'exit':
         print('Спасибо за использование! До свидания!')
         break
